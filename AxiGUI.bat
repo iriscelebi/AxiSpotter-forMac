35ed2c449f4de6c78e771ad6ae4b7fb987753a34
Iris Celebi <51130468+iriscelebi@users.noreply.github.com>
2022-07-08 15:01:18 -0400

    Add files via upload

---
+++ AxiGUI.bat
@@ -0,0 +1,12 @@
+@echo OFF
+
+call D:\Venv374Axi\Scripts\activate
+
+rem Run a python script in that environment
+
+python C:\Users\irisclb\Desktop\AxiSpotter-forMac-main\Standalone_Spotexp_Simple.py
+
+rem Deactivate the environment
+call deactivate
+
+pause
